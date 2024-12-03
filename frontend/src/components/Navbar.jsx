28faf53b27188aa0d0d2203d881512a8d7bef18b
Nesrin D. Tanyeri <daghantanyeri@gmail.com>
2024-12-03 13:33:37 +0100

    fixed homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,15 @@
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 border border-base-300 hover:bg-base-200">
+        {/* Logo Section */}
+        <div className="flex-1">
+          <Link className="btn btn-ghost normal-case text-xl" to="/">
+            Pokemon Battles
+          </Link>
+        </div>
     return (
         <div className="navbar bg-base-100 border border-base-300 hover:bg-base-200">
             {/* Logo Section */}
@@ -8,29 +17,54 @@ const Navbar = () => {
                 <Link className="btn btn-ghost normal-case text-xl" to="/">Pokemon Battles</Link>
             </div>
 
-            {/* Navigation Links */}
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li>
-                        <NavLink 
-                            to="/" 
-                            className={({ isActive }) => isActive ? "text-primary font-bold" : ""}
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to="/leaderboard" 
-                            className={({ isActive }) => isActive ? "text-primary font-bold" : ""}
-                        >
-                            Leaderboard
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
+        {/* Navigation Links */}
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "text-primary font-bold" : ""
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/battle"
+                className={({ isActive }) =>
+                  isActive ? "text-primary font-bold" : ""
+                }
+              >
+                Battle
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/myRoster"
+                className={({ isActive }) =>
+                  isActive ? "text-primary font-bold" : ""
+                }
+              >
+                My Roster
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/leaderboard"
+                className={({ isActive }) =>
+                  isActive ? "text-primary font-bold" : ""
+                }
+              >
+                Leaderboard
+              </NavLink>
+            </li>
+          </ul>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
